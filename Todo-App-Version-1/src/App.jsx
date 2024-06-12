13b1c2b7874dde3a42471eff19d358ece772a291
Asthaviradiya <asthaviradiya114@gmail.com>
2024-06-12 08:40:42 +0530

    app jsx file is modified

--- Todo-App-Version-1/src/App.jsx
+++ Todo-App-Version-1/src/App.jsx
@@ -1,22 +1,13 @@
 import AppName from "/Users/Asus/github-repo/Todo-App/Todo-App-Version-1/src/AppName"
 import AddTodo from "/Users/Asus/github-repo/Todo-App/Todo-App-Version-1/src/AddTodo"
+import TodoItem1 from "/Users/Asus/github-repo/Todo-App/Todo-App-Version-1/src/TodoItem1"
 function App() {
   return <center class="todo-container">
 
     <AppName></AppName>
     <AddTodo></AddTodo>
+    <TodoItem1></TodoItem1>
     <div class="container text-center">
-      <div class="row">
-        <div class="col-6">
-          Buy Milk
-        </div>
-        <div class="col-4">
-          4/10/2023
-        </div>
-        <div class="col-2">
-          <button type="button" class="btn btn-danger">delete</button>
-          </div>
-      </div>
       <div class="row">
         <div class="col-6">
           Go To College
