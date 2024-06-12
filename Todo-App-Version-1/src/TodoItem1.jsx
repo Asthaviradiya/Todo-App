ae0e5603ea1f0a83e9fa719c781eb60d83d935ae
Asthaviradiya <asthaviradiya114@gmail.com>
2024-06-12 08:43:16 +0530

    todoitem1 jsx file is modified

--- Todo-App-Version-1/src/TodoItem1.jsx
+++ Todo-App-Version-1/src/TodoItem1.jsx
@@ -1,11 +1,14 @@
 function TodoItem(){
+    let name = 'Buy Milk';
+    let date = ' 4/10/2023'
+
     return <div class="container text-center">
     <div class="row">
       <div class="col-6">
-        Buy Milk
+        {name}
       </div>
       <div class="col-4">
-        4/10/2023
+       {date}
       </div>
       <div class="col-2">
         <button type="button" class="btn btn-danger">delete</button>
